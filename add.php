9920cc08c1f232183ebaa2eca6ce2ef2f031611b
Antonio Delvair Zaneti <antonio.zaneti@gmail.com>
2025-11-13 17:27:27 +0000

    Transformando em Página

--- add.php
+++ add.php
@@ -1,5 +1,28 @@
-<?php 
-  $a=5;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  
+  <style>
+    #container{
+      width: 70%;
+      margin: auto;
+      text-align: center;
+      border: 2px solid black;
+      padding: 20px;
+      box-shadow: 5px 10px #888888;
+    }
+  </style>
+
+  }
+</head>
+<body>
+  <div id="container">
+    <h1>PHP Addition and Subtraction</h1>
+    <?php 
+    
   $b=10;
   $res = $a + $b;
   echo ("$a + $b =  $res");
@@ -8,3 +31,6 @@
   $sub = $d - $c;
   echo ("$d-$b = $sub");
 ?>
+</div>
+</body>
+</html>
